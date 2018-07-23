f7528f23586d6f91e43bde0a47fe177ac30f190e
Alex Hope <alex.hopee@gmail.com>
2018-07-23 23:16:55 +0100

    Updated test enemy (missed from prev. commit?)

--- Assets/_Scripts/Enemy/TestEnemy.cs
+++ Assets/_Scripts/Enemy/TestEnemy.cs
@@ -5,11 +5,9 @@ Created by Alex Hope. For use in the Project-TD app.
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Pathfinding;
 
-public class TestAttack : Attack
+public class TestEnemy : Enemy
 {
-	public override void AttackTarget(GameObject target)
-    {
 
-    }
 }
